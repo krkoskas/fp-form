dec0bc8f4355525dd61ff10a42306fb0de2d40b2
Susanne Krkoska <zuzka.krkoska@seznam.cz>
2023-11-05 17:39:11 +0100

    v. 1.2, ErrorMessage styling added

--- src/components/ErrorMessage/index.jsx
+++ src/components/ErrorMessage/index.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import "./style.css";
 
-const ErrorMessage = ({lastname, email, invalidemail}) => {
-
-    return (
-        <div className="error_wrapper">
-            {lastname ? <p className="error_message">Please enter your surname.</p> :null }
-            {email ? <p className="error_message">Please enter your email.</p> :null }
-            {invalidemail ? <p className="error_message">Please enter a valid email.</p> :null }
+const ErrorMessage = ({ lastname, email, invalidemail }) => {
+  return (
+    <div className="error_wrapper">
+      <div className="error_bar"></div>
+        <div className="error_status">
+          {lastname ? (
+            <p className="error_message">Please enter your surname.</p>
+          ) : null}
+          {email ? (
+            <p className="error_message">Please enter your email.</p>
+          ) : null}
+          {invalidemail ? (
+            <p className="error_message">Please enter a valid email.</p>
+          ) : null}
         </div>
-    )
 
-}
+    </div>
+  );
+};
 
-export default ErrorMessage
+export default ErrorMessage;
--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 
-const Results = ({ firstname, lastname, email, linkedin, motivation, skills }) => {
+const Results = ({
+  firstname,
+  lastname,
+  email,
+  linkedin,
+  motivation,
+  skills,
+}) => {
   return (
     <div className="results_wrapper">
-      {firstname}
-      {lastname}
-      {email}
-      {linkedin}
-      {motivation}
-      {skills}
+      <div className="results_bar"></div>
+      <div className="results_status">
+        <h2 className="results_heading">Thank you for your application!</h2>
+        <p>First Name: {firstname}</p>
+        <p>Last Name: {lastname}</p>
+        <p>Email Address: {email}</p>
+        <p>LinkedIn URL: {linkedin}</p>
+        <p>Why do you want to become a part of the Femme Palette team? {motivation}</p>
+        <p>Why are you the best candidate for this role? {skills}</p>
+      </div>
     </div>
   );
 };
